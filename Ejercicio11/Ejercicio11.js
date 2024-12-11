65c3475d0c7dfa18b59f114cb602622cec318acd
Chino <88289548+crespoc@users.noreply.github.com>
2024-12-11 03:25:10 +0000

    ejercicio11

---
+++ Ejercicio11/Ejercicio11.js
@@ -0,0 +1,6 @@
+const base = parseFloat(prompt('ingrese la base en metros'));
+const altura = parseFloat(prompt('ingrese la altura en metros'));
+
+let superficie = base * altura;
+
+console.log(superficie);
