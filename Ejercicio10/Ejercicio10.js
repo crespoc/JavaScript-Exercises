c0723ed04c51d8f5297c1b84fd65a636863c599b
Chino <88289548+crespoc@users.noreply.github.com>
2024-12-11 03:19:40 +0000

    ejercicio10

---
+++ Ejercicio10/Ejercicio10.js
@@ -0,0 +1,13 @@
+let variable;
+
+variable = false;
+console.log(variable);
+
+variable = "Hola";
+console.log(variable);
+
+variable = 0;
+console.log(variable);
+
+variable = "Minios";
+console.log(variable);
